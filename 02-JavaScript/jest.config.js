c698d9ed2691899305ce97eca30e99f002334931
jaehkim <jaeh.kim1209@gmail.com>
2026-07-09 14:34:54 +0900

    Remove 02-JavaScript directory

--- 02-JavaScript/jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  testEnvironment: "node",
-  testMatch: ["**/*.test.js", "**/*.test.ts"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  verbose: true,
-};
